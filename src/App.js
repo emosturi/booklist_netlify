a3ce368f9c6eff51b5766359cc6f6d728153cfdf
emosturi <nicsalb@gmail.com>
2022-06-12 18:00:19 -0400

    migrated styles from css to globalStyle file using styled components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import GlobalStyle from './globalStyle';
 import BookContextProvider from './contexts/BookContext';
 import Navbar from './components/Navbar';
 import BookList from './components/BookList';
 import BookForm from './components/BookForm';
 
+
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <BookContextProvider>
         <Navbar />
         <BookList />
