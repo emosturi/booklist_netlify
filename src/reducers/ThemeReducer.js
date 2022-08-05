21133d4b7c304b69189c800fc4c2d4f5768e90cc
emosturi <nicsalb@gmail.com>
2022-08-05 13:34:17 -0400

    Feature - Added Theme toggler to the local storage

--- src/reducers/ThemeReducer.js
+++ src/reducers/ThemeReducer.js
@@ -1,9 +1,8 @@
 export const ThemeReducer = (state, action) => {
-  switch (action.type) {
-    case 'TOGGLE_THEME':
-      return action.isDarkTheme
-      break;
-    default:
-      return state
-  }
-}
+    switch (action.type) {
+        case "TOGGLE_THEME":
+            return action.isDarkTheme;
+        default:
+            return state;
+    }
+};
